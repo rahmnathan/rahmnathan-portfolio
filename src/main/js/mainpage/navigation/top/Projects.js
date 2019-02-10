4581f19736fcbe622f49758fa069a58222480ebf
nathan <rahm.nathan@gmail.com>
2019-02-09 22:36:07 -0600

    Added hover styling to navigation links.

--- src/main/js/mainpage/navigation/top/Projects.js
+++ src/main/js/mainpage/navigation/top/Projects.js
@@ -13,11 +13,40 @@ const projectsStyle = {
     textDecoration: 'none'
 };
 
+const hoveredProjectsStyle = {
+    margin: 8,
+    fontSize: 19,
+    color: 'gray'
+};
+
 export class Projects extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state = { hovered: false };
+        this.addHover = this.addHover.bind(this);
+        this.removeHover = this.removeHover.bind(this);
+    }
+
+    addHover(){
+        this.setState({ hovered: true });
+    }
+
+    removeHover(){
+        this.setState( { hovered: false });
+    }
+
     render() {
+        if(this.state.hovered !== null && this.state.hovered === true){
+            return (
+                <div onMouseEnter={this.addHover} onMouseLeave={this.removeHover} style={divStyle}>
+                    <Link to='/projects' style={hoveredProjectsStyle}>Projects</Link>
+                </div>
+            );
+        }
+
         return (
-            <div style={divStyle}>
+            <div onMouseEnter={this.addHover} onMouseLeave={this.removeHover} style={divStyle}>
                 <Link to='/projects' style={projectsStyle}>Projects</Link>
             </div>
         );
--- src/main/js/mainpage/navigation/top/Summary.js
+++ src/main/js/mainpage/navigation/top/Summary.js
@@ -6,17 +6,48 @@ const divStyle = {
     float: 'right'
 };
 
-const projectsStyle = {
+const summaryStyle = {
     margin: 8,
     fontSize: 19,
     color: 'black',
     textDecoration: 'none'
 };
 
-export const Summary = () => {
-    return (
-        <div style={divStyle}>
-            <Link to='/' style={projectsStyle}>Summary</Link>
-        </div>
-    );
+const hoveredSummaryStyle = {
+    margin: 8,
+    fontSize: 19,
+    color: 'gray'
+};
+export class Summary extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = { hovered: false };
+        this.addHover = this.addHover.bind(this);
+        this.removeHover = this.removeHover.bind(this);
+    }
+
+    addHover(){
+        this.setState({ hovered: true });
+    }
+
+    removeHover(){
+        this.setState( { hovered: false });
+    }
+
+    render() {
+        if(this.state.hovered !== null && this.state.hovered === true){
+            return (
+                <div onMouseEnter={this.addHover} onMouseLeave={this.removeHover} style={divStyle}>
+                    <Link to='/' style={hoveredSummaryStyle}>Summary</Link>
+                </div>
+            );
+        }
+
+        return (
+            <div onMouseEnter={this.addHover} onMouseLeave={this.removeHover} style={divStyle}>
+                <Link to='/' style={summaryStyle}>Summary</Link>
+            </div>
+        );
+    }
 };
