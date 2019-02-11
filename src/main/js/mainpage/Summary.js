2a80e0d50051ddfe96d10d6e7724d5646101de94
nathan <rahm.nathan@gmail.com>
2019-02-10 21:21:59 -0600

    Rephrased summary statement.

--- src/main/js/mainpage/Summary.js
+++ src/main/js/mainpage/Summary.js
@@ -28,7 +28,7 @@ export const Summary = () => {
         <div style={summaryStyle}>
             <div style={summaryTextWrapperStyle}>
                 <p style={summaryTextStyle}>Hello, I'm a Software Engineer in Minneapolis, MN.</p>
-                <p style={summaryTextStyle}>I design, build, deploy and monitor systems to service business needs.</p>
+                <p style={summaryTextStyle}>I design, build, deploy and monitor systems that provide high quality user experiences.</p>
             </div>
         </div>
     );
