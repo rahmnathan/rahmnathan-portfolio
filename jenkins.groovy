94b4fdd5ee467d10c32e55f27578639473bc2edd
nathan <rahm.nathan@gmail.com>
2020-08-10 18:21:10 -0500

    Added Helm Chart. Added Jenkins pipeline file. Updated Spring version.

---
+++ jenkins.groovy
@@ -0,0 +1,51 @@
+node {
+    def mvnHome
+    def jdk
+    stage('Setup') {
+        mvnHome = tool 'Maven'
+        jdk = tool name: 'Java 14'
+        env.JAVA_HOME = "${jdk}"
+    }
+    stage('Checkout') {
+        git 'https://github.com/rahmnathan/rahmnathan-portfolio.git'
+    }
+    stage('Set Version') {
+        PROJECT_VERSION = sh(
+                script: "'${mvnHome}/bin/mvn' help:evaluate -Dexpression=project.version -q -DforceStdout",
+                returnStdout: true
+        ).trim()
+        env.NEW_VERSION = "${PROJECT_VERSION}.${BUILD_NUMBER}"
+        sh "'${mvnHome}/bin/mvn' -DnewVersion='${NEW_VERSION}' versions:set"
+    }
+    stage('Tag') {
+        sh 'git config --global user.email "rahm.nathan@gmail.com"'
+        sh 'git config --global user.name "rahmnathan"'
+        sshagent(credentials: ['Github-Git']) {
+            sh 'mkdir -p /home/jenkins/.ssh'
+            sh 'ssh-keyscan  github.com >> ~/.ssh/known_hosts'
+            sh "'${mvnHome}/bin/mvn' -Dtag=${NEW_VERSION} scm:tag"
+        }
+    }
+    stage('Package') {
+        sh "'${mvnHome}/bin/mvn' clean install -DskipTests"
+    }
+    stage('Test') {
+        sh "'${mvnHome}/bin/mvn' test"
+    }
+    stage('Docker Build') {
+        sh "'${mvnHome}/bin/mvn' dockerfile:build"
+    }
+    withCredentials([[$class : 'UsernamePasswordMultiBinding', credentialsId: 'Dockerhub',
+                      usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+        stage('Docker Push') {
+            sh "'${mvnHome}/bin/mvn' dockerfile:push -Ddockerfile.username=$USERNAME -Ddockerfile.password='$PASSWORD'"
+        }
+    }
+    stage('Deploy to Kubernetes') {
+        withCredentials([
+                file(credentialsId: 'Kubeconfig', variable: 'KUBE_CONFIG'),
+        ]) {
+            sh 'helm upgrade --install rahmnathan-portfolio ./target/classes/rahmnathan-portfolio/ --kubeconfig $KUBE_CONFIG'
+        }
+    }
+}
--- src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module rahmnathan.portfolio {
-    requires spring.boot.autoconfigure;
-    requires spring.web;
-    requires spring.boot;
-    requires spring.context;
-}
