652bea93ad252a72513d72f86e2c9f99c7d92d09
nathan <rahm.nathan@gmail.com>
2020-08-10 19:43:24 -0500

    Fixed links.

--- src/main/js/projects/localmovies/LocalMovies.js
+++ src/main/js/projects/localmovies/LocalMovies.js
@@ -25,8 +25,8 @@ export const LocalMovies = () => {
             <p style={titleStyle}>LocalMovies</p>
             <div style={{ display: 'inline-block', width: '100%', textAlign: 'center' }}>
                 <p style={descriptionStyle}>This project is a media server with both web and Android clients.</p>
-                <p><a  style={descriptionStyle} href="https://nathanrahm-grafana.ddns.net">View live monitoring with Grafana.</a></p>
-                <p><a  style={descriptionStyle} href="https://github.com/rahmnathan/localmovie-web-service">View the source on Github. </a></p>
+                <p><a  style={descriptionStyle} href="https://grafana.nathanrahm.com/d/kjj-uuJWk/localmovies?orgId=1">View live monitoring with Grafana.</a></p>
+                <p><a  style={descriptionStyle} href="https://github.com/rahmnathan/localmovie-media-manager">View the source on Github. </a></p>
             </div>
             <Webapp/>
             <AndroidApp/>
